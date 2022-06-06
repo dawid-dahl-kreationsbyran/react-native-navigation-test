9a64cfe0af487590e0eb9e5b98891bc63e6202aa
Dawid-Dahl <dawid.dahl@kreationsbyran.se>
2022-06-06 18:47:19 +0200

    fix dependencies

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
+import "react-native-gesture-handler"
 import { StatusBar } from "expo-status-bar"
-import { Button, StyleSheet } from "react-native"
+import { StyleSheet } from "react-native"
 import CategoriesScreen from "./screens/CategoriesScreen"
 import { NavigationContainer } from "@react-navigation/native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import MealsOverviewScreen from "./screens/MealsOverviewScreen"
 import MealDetailScreen from "./screens/MealDetailScreen"
 import IconButton from "./components/IconButton"
+import { createDrawerNavigator } from "@react-navigation/drawer"
 
 const Stack = createNativeStackNavigator()
+const Drawer = createDrawerNavigator()
 
 const screenHeaderOptions = {
 	title: "Awesome Title",
